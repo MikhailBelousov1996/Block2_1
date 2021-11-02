01336d5649e00a1afe7f576eef6c73183565dde4
Mihal Belousov <wroomwroom1996@gmail.com>
2021-11-02 19:29:56 +0300

    add code

---
+++ untitled/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int price = 200;
+        int milesFromTicket = 20;
+        int numberMiles = price / milesFromTicket;
+        System.out.println("Вам начислено - " + milesFromTicket + " " + "мили");
+    }
+}
